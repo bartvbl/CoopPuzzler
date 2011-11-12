740ce5c6113ba140a0a957b0ecae1f820673745c
Bart van Blokland <bartvblokl@gmail.com>
2011-11-12 16:43:24 +0100

    added lwjgl

--- CoopPuzzler/src/client/ClientMain.java
+++ CoopPuzzler/src/client/ClientMain.java
@@ -1,5 +1,17 @@
 package client;
 
 public class ClientMain {
+	private ClientWindow window;
+	
+	public ClientMain()
+	{
+		this.window = new ClientWindow(this);
+		this.window.mainLoop();
+	}
+	
+	public void doFrame() {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
