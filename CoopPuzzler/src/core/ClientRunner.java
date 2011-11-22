9d3a8ef6e28145e5b93242b3f91f6640f8099008
Bart van Blokland <bartiver@stud.ntnu.no>
2011-11-22 18:37:32 +0100

    added natives for linux and mac OSX

---
+++ CoopPuzzler/src/core/ClientRunner.java
@@ -0,0 +1,23 @@
+package core;
+import javax.swing.JOptionPane;
+
+import client.ClientMain;
+
+
+public class ClientRunner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		try{
+			new ClientMain();
+		}
+		catch(Exception e)
+		{
+			JOptionPane.showMessageDialog(null, "launch failed: " + e.getMessage());
+		}
+		
+	}
+
+}
